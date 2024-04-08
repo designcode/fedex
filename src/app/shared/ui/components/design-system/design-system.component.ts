569cdf76fe06ca796bc04e2982924f42040a32d4
Abdullah Ibrahim <hello@designcode.me>
2024-04-08 10:56:50 +0200

    feat: Creates a basic design system

---
+++ src/app/shared/ui/components/design-system/design-system.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-fedex-design-system',
+	standalone: true,
+	templateUrl: './design-system.component.html',
+})
+export class DesignSystemComponent {}
---
+++ src/app/shared/ui/components/index.ts
@@ -0,0 +1,1 @@
+export * from './design-system/design-system.component';
