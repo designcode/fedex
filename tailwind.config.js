d0e429e81cdf0518081d4e74fd663044105c6145
Abdullah Ibrahim <hello@designcode.me>
2024-04-08 10:11:14 +0200

    styles: Implements base styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
 	content: ['./src/**/*.{html,ts}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				white: '#ffffff',
+				grey: '#333',
+				primary: '#4d148c',
+				secondary: '#ff6200',
+				warning: '#de002e',
+			},
+		},
 	},
 	plugins: [],
 };
